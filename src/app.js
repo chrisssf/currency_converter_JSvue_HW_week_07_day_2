6636bc22b428dbeab86f0cdb8fd211ed6724a1b0
Chris Fraser <chrisssfraser@gmail.com>
2020-01-15 07:31:56 +0000

    mvp and first extension completed

--- src/app.js
+++ src/app.js
@@ -4,10 +4,44 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-
+      exchangeData: {},
+      selectedRate: {},
+      amount: null,
+      answer: null,
+      direction: null,
+      currency: null,
+      desiredRate: null
+    },
+    mounted: function () {
+      this.getRates()
     },
     methods: {
+      getRates: function(){
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(resource => resource.json())
+        .then(rates => this.exchangeData = rates)
+      },
+      calculateNewAmount: function() {
+        // console.log("thing works");
+        if (this.direction === "fromEuros"){
+          const total = this.selectedRate.rate * this.amount
+          this.answer = Math.round(total * 100) / 100
+        } else {
+          const total = this.amount / this.selectedRate.rate
+          this.answer = total.toFixed(2)
+        }
+      },
+      getCurrency: function() {
+        this.currency = this.selectedRate.currency
+      },
+      convert: function() {
+        const total = this.amount / this.selectedRate.rate
+        // this.answer = Math.round(total * 100) / 100
+        const result = this.desiredRate.rate * total
+        // this.answer = Math.round(result * 100) / 100
+        this.answer = result.toFixed(2)
 
+      }
     }
   });
 });
